651ac64d9e585b4b1e351de58abf595f0525edf5
Abdul Moiz <moiza0799@gmail.com>
2024-10-01 11:46:25 -0500

    Initial commit

---
+++ src/main/java/com/example/sprintbootapp/SprintBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootAppApplication.class, args);
+    }
+
+}
